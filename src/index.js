4d235c44787178cec5d26038f2199ee5d3627b1f
MrSpunki <36659232+MrSpunki@users.noreply.github.com>
2018-09-24 18:58:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  
+    var roots = [];
+  
+  var parts = equation.split(" ");
+  //console.log(parts);
+  
+  var discriminant = parts[4] * parts[4] - 4 * parts[0] * parts[8];
+  //console.log("discriminant " + discriminant);
+  
+  if (discriminant === 0){
+    var x = Math.round((-1*parts[4])/(2*parts[0]));
+    roots.push(x);
+    
+  }else if (discriminant > 0) {
+    var x1 = Math.round((-1*parts[4]+Math.sqrt(discriminant))/(2*parts[0]));
+    var x2 = Math.round((-1*parts[4]-Math.sqrt(discriminant))/(2*parts[0]));
+    roots.push(x1,x2);
+    
+  }else {
+    roots.push(0);
+  }
+  
+  roots.sort();
+  //console.log("roots " + roots);
 }
